af4ea9021f17334d75b8d5b212024a26de40b54a
perekotypole <parcersara@gmail.com>
2024-07-23 17:42:30 +0300

    query actors budgets

---
+++ 1_actors_budgets.sql
@@ -0,0 +1,5 @@
+SELECT p.id, p.first_name, p.last_name, SUM(m.budget) as total_budget
+FROM people p
+JOIN movies_roles mr ON p.id = mr.person_id
+JOIN movies m ON mr.movie_id = m.id
+GROUP BY p.id, p.first_name, p.last_name;
